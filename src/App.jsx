e00d742c04ad2c6234acb75cb3001641004165ca
Abhishek kumar <abhi.arya97161@gmail.com>
2025-01-18 13:28:16 +0530

    add state management and refs for map interactions in App component

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,17 @@ import { useState } from "react";
 import { useRef } from "react";
 
 function App() {
-  
+  const mapElement = useRef(null);
+  const [isFirstClick, setIsFirstClick] = useState(true);
+  const [map, setMap] = useState(null);
+  const [vectorSource] = useState(new VectorSource());
+  const [isDrawing, setIsDrawing] = useState(false);
+  const [lineStringModalOpen, setLineStringModalOpen] = useState(false);
+  const [polygonModalOpen, setPolygonModalOpen] = useState(false);
+  const [lineStringCoordinates, setLineStringCoordinates] = useState([]);
+  const [polygonCoordinates, setPolygonCoordinates] = useState([]);
+  const [dropdownIndex, setDropdownIndex] = useState(null);
+
   return (
     <>
       <div className="underline text-sm text-indigo-400">
