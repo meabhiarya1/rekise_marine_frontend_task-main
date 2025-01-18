5561b8f59045b88e315d2f9a07201d32271272e4
Abhishek kumar <abhi.arya97161@gmail.com>
2025-01-18 13:27:50 +0530

    add OpenLayers integration and update App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import "./App.css";
+import "ol/ol.css";
+import { Map, View } from "ol";
+import { Tile as TileLayer } from "ol/layer";
+import { OSM } from "ol/source";
+import { Draw } from "ol/interaction";
+import { Vector as VectorLayer } from "ol/layer";
+import { Vector as VectorSource } from "ol/source";
+import { useState } from "react";
+import { useRef } from "react";
 
 function App() {
-
+  
   return (
     <>
       <div className="underline text-sm text-indigo-400">
