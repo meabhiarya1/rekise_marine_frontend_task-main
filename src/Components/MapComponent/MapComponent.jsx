736206565301232aadf2bd1535088377109db684
Abhishek kumar <abhi.arya97161@gmail.com>
2025-01-19 16:05:57 +0530

    feat: refactor MapComponent to use forwardRef and add drawing/modal interaction buttons

--- src/Components/MapComponent/MapComponent.jsx
+++ src/Components/MapComponent/MapComponent.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { forwardRef } from "react";
 
-const MapComponent = () => {
-  return (
-    <div>
-      MapComponent
-    </div>
-  )
-}
-
-export default MapComponent
+export const MapComponent = forwardRef(
+  ({ handleDrawingClick, handleModalOpen, isFirstClick }, ref) => {
+    return (
+      <main ref={ref} className="flex-grow relative">
+        <div className="absolute top-0 left-0 w-full" style={{ zIndex: 1 }}>
+          <header className="text-white p-4 flex justify-end items-center">
+            <div className="space-x-4">
+              {!isFirstClick && (
+                <button
+                  className="bg-blue-500 px-4 py-2 rounded text-white hover:bg-blue-600"
+                  onClick={handleModalOpen}
+                >
+                  Open Mission Creation Modal
+                </button>
+              )}
+              <button
+                className="bg-blue-500 px-4 py-2 rounded text-white hover:bg-blue-600"
+                onClick={handleDrawingClick}
+              >
+                Click Here To Draw on the Map
+              </button>
+            </div>
+          </header>
+        </div>
+      </main>
+    );
+  }
+);
